a512aaad4455b19d2478c627f4837cb6917b425c
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-07-19 13:49:56 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -11,7 +11,7 @@ def main():
         <style>
         .stApp {
             background-color: orange;
-            color: black;
+            color: #05DECA;
         }
         .stTextInput>div>div>input {
             background-color: black;
