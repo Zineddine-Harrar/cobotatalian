1ecf84946a7b58b15d4c3ddce86700a76d31c450
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-07-27 21:47:05 +0200

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -97,11 +97,11 @@ def main():
     if 'selected_app' not in st.session_state:
         st.session_state['selected_app'] = None
 
-    query_params = st.experimental_get_query_params()
-    if 'logged_in' in query_params and query_params['logged_in'][0] == 'true':
+    query_params = st.query_params
+    if 'logged_in' in query_params and query_params['logged_in'] == 'true':
         st.session_state['logged_in'] = True
 
-    if 'logged_in' in query_params and query_params['logged_in'][0] == 'false':
+    if 'logged_in' in query_params and query_params['logged_in'] == 'false':
         st.session_state['logged_in'] = False
 
     if st.session_state['logged_in']:
@@ -121,6 +121,7 @@ def login_section():
             st.success(f"Bienvenue {username}")
             st.session_state['logged_in'] = True
             st.session_state['username'] = username
+            st.query_params.update({"logged_in": "true"})
             js_redirect({"logged_in": "true"})
         else:
             st.error("Nom d'utilisateur ou mot de passe incorrect")
@@ -135,6 +136,7 @@ def app_selection_page():
         st.session_state['logged_in'] = False
         st.session_state['username'] = ''
         st.session_state['selected_app'] = None
+        st.query_params.update({"logged_in": "false"})
         js_redirect({"logged_in": "false"})
 
     st.markdown("### Sélectionnez une application")
@@ -143,16 +145,19 @@ def app_selection_page():
     with col1:
         if st.button("RQUARTZ - IMON"):
             st.session_state['selected_app'] = "RQUARTZ - IMON"
+            st.query_params.update({"app": "rquartz_imon"})
             js_redirect({"app": "rquartz_imon"})
 
     with col2:
         if st.button("RQUARTZ - T2F"):
             st.session_state['selected_app'] = "RQUARTZ - T2F"
+            st.query_params.update({"app": "rquartz_t2f"})
             js_redirect({"app": "rquartz_t2f"})
             
     with col3:
         if st.button("ECOBOT 40"):
             st.session_state['selected_app'] = "ECOBOT 40"
+            st.query_params.update({"app": "ecobot_40"})
             js_redirect({"app": "ecobot_40"})
 
     if st.session_state['selected_app']:
