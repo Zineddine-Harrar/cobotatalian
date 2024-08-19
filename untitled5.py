5c3f73bea83479d71818c3c418a62eafe55ede13
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-08-19 15:22:30 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -93,7 +93,7 @@ def main():
 
     # Charger les fichiers CSV
     planning_df = pd.read_csv('PLANNING RQUARTZ IMON  (1).csv', delimiter=';', encoding='ISO-8859-1')
-    details_df = pd.read_csv('DATASET/IMON/12-08.csv', encoding='ISO-8859-1', delimiter=';', on_bad_lines='skip')
+    details_df = pd.read_csv('DATASET/IMON/19-08.csv', encoding='ISO-8859-1', delimiter=';', on_bad_lines='skip')
     
     # Nettoyer les colonnes dans details_df
     details_df.columns = details_df.columns.str.replace('\r\n', '').str.strip()
@@ -266,7 +266,7 @@ def main():
         return weekly_cost, hourly_cost, total_cost, utilization_rate
     description_evenements = pd.read_excel("Description des evenements.xlsx")
     # Load the dataset with appropriate header row
-    file_path = "DATASET/ALERTE/IMON/12-08.xlsx"
+    file_path = "DATASET/ALERTE/IMON/Alerte IMON  19-08.xlsx"
     alarm_details_df = pd.read_excel(file_path, header=4)
    # Rename columns for easier access
     alarm_details_df.columns = ['Index', 'Code', 'Composant', 'Description', 'Apparition_Date', 'Apparition_Time', 'Retour_Date', 'Retour_Time', 'Modèle_machine', 'Machine_Description', 'N_de_série']
