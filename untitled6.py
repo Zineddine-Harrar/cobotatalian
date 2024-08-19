b3d8f0a5b5e64021bd9bad7bac878e27131f87cf
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-08-19 15:36:39 +0200

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -92,7 +92,7 @@ def main():
 
     # Charger les fichiers CSV
     planning_df = pd.read_csv('PLANNING RQUARTZ T2F.csv', delimiter=';', encoding='ISO-8859-1')
-    details_df = pd.read_csv('DATASET/T2F/05-08.csv', encoding='ISO-8859-1', delimiter=';', on_bad_lines='skip')
+    details_df = pd.read_csv('DATASET/T2F/19-08.csv', encoding='ISO-8859-1', delimiter=';', on_bad_lines='skip')
 
     # Nettoyer les colonnes dans details_df
     details_df.columns = details_df.columns.str.replace('\r\n', '').str.strip()
@@ -263,7 +263,7 @@ def main():
         return weekly_cost, hourly_cost, total_cost, utilization_rate
         
     # Load the dataset with appropriate header row
-    file_path = "DATASET/ALERTE/T2F/Alerte T2F  05-08.xlsx"
+    file_path = "DATASET/ALERTE/T2F/Alerte T2F  19-08.xlsx"
     alarm_details_df = pd.read_excel(file_path, header=4)
     
     # Rename columns for easier access
