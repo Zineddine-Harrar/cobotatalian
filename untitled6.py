3d68982a013cf85469c3ab623998a9f70f4ee49d
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-08-23 14:48:21 +0200

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -366,9 +366,7 @@ def main():
         else:
             filtered_data = filtered_alarm_details_df
     
-        # Afficher un échantillon des données filtrées pour vérification
-        st.write("Échantillon des données filtrées:")
-        st.write(filtered_data[['Apparition', 'Catégorie']].head())
+       
 
         # Calculer le nombre total d'alertes pour la semaine
         total_alerts_week = len(filtered_data)
@@ -682,9 +680,7 @@ def main():
         else:
             filtered_data = filtered_alarm_details_df
 
-        # Afficher un échantillon des données filtrées pour vérification
-        st.write("Échantillon des données filtrées:")
-        st.write(filtered_data[['Apparition', 'Catégorie']].head())
+       
         # Calculer le nombre total d'alertes pour le mois
         total_alerts_month = len(filtered_data)
 
