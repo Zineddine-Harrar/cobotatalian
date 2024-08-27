ff74357307394a25eb5753868bccc086a89974fe
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-08-27 14:34:33 +0200

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -8,15 +8,7 @@ import plotly.io as pio
 from plotly.subplots import make_subplots
 import openpyxl
 import io
-# Au début de chaque application (RQUARTZ IMON, RQUARTZ T2F, ECOBOT 40)
-def initialize_app_state(app_name):
-    if 'current_app' not in st.session_state:
-        st.session_state.current_app = app_name
 
-    if 'last_app' not in st.session_state or st.session_state.last_app != app_name:
-        st.session_state.last_app = app_name
-        # Réinitialiser le filtre de sélection à "Semaine"
-        st.session_state[f'period_selection_{app_name}'] = "Semaine"
 def main():
 
     st.markdown(
@@ -338,17 +330,16 @@ def main():
 
     # Pour RQUARTZ T2F
     
-    initialize_app_state("RQUARTZ_T2F")
-        
+    if 'period_selection' not in st.session_state:
+        st.session_state['period_selection'] = "Semaine"
+
     period_selection = st.radio(
-         "Sélectionnez la période à analyser", 
-         ["Semaine", "Mois"], 
-         key="period_selection_RQUARTZ_T2F",
-         index=0 if st.session_state.period_selection_RQUARTZ_T2F == "Semaine" else 1
+        "Sélectionnez la période à analyser",
+        ["Semaine", "Mois"],
+        index=["Semaine", "Mois"].index(st.session_state['period_selection'])
     )
-        
-    # Mettre à jour la valeur dans session_state
-    st.session_state.period_selection_RQUARTZ_T2F = period_selection
+
+    st.session_state['period_selection'] = period_selection
     if period_selection == "Semaine":
         selected_week = st.selectbox("Sélectionnez le numéro de la semaine", options=list(week_options.keys()), format_func=lambda x: f"Semaine {x} ({week_options[x].strftime('%d/%m/%Y')})")
 
