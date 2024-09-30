0dbc246074d25e6080f8edd12574dfbace7a47bb
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-09-30 15:28:53 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -112,7 +112,7 @@ def main():
 
 
     # Charger les fichiers CSV
-    planning_df = pd.read_csv('PLANNING RQUARTZ IMON  (1).csv', delimiter=';', encoding='ISO-8859-1')
+    planning_df = pd.read_csv('PLANNING RQUARTZ IMON (1).csv', delimiter=';', encoding='ISO-8859-1')
     details_df = pd.read_csv('DATASET/IMON/30-08.csv', encoding='ISO-8859-1', delimiter=';', on_bad_lines='skip')
     
     # Nettoyer les colonnes dans details_df
