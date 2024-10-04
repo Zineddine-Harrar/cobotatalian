801572021d4cd12dbd66e0fc99fca0d3893e075d
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-10-04 11:28:52 +0200

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -223,9 +223,7 @@ def main():
 
 
 
-    matin = ['F14 Pt9 H', 'Triplex 6d F14', 'Pt 3-5d Triplex']
-    apres_midi = ['Pt 14d Triplex', 'Porte 1-3d H', 'Porte 9-11d H']
-    soir = ['Triplex 17d H', 'Pt 12-14d H']
+
     
     # Fonction pour calculer le taux de suivi à partir du tableau de suivi
     def calculate_taux_suivi_from_table(comparison_table):
