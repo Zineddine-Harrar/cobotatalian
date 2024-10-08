bb96983d0db2537861a9a2c4c0d76cbf209316b0
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-10-08 09:06:36 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -162,8 +162,8 @@ def main():
     # Fonction pour nettoyer les doublons
     def clean_duplicates(details_df):
         # Convertir les colonnes "début" et "fin" en format datetime
-        details_df['début'] = pd.to_datetime(details_df['début'], format='%d/%m/%Y %H:%M:%S', errors='coerce')
-        details_df['fin'] = pd.to_datetime(details_df['fin'], format='%d/%m/%Y %H:%M:%S', errors='coerce')
+        details_df['début'] = pd.to_datetime(details_df['début'], format='%d/%m/%Y %H:%M', errors='coerce')
+        details_df['fin'] = pd.to_datetime(details_df['fin'], format='%d/%m/%Y %H:%M', errors='coerce')
     
         # Extraire la date de début
         details_df['date'] = details_df['début'].dt.date
