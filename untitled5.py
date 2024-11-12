549578b365a4c648258744220b72556d7c0d0da4
Zineddine-Harrar <123366772+Zineddine-Harrar@users.noreply.github.com>
2024-11-12 10:59:54 +0100

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -313,7 +313,7 @@ def main():
         return weekly_cost, hourly_cost, total_cost, utilization_rate
     description_evenements = pd.read_excel("Description des evenements.xlsx")
     # Load the dataset with appropriate header row
-    file_path = "DATASET/ALERTE/IMON/Alerte IMON 05-11.xlsx"
+    file_path = "DATASET/ALERTE/IMON/Alerte IMON 12-11.xlsx"
     alarm_details_df = pd.read_excel(file_path, header=4)
     description_evenements = pd.read_excel("Description des evenements.xlsx")
     # Rename columns for easier access
